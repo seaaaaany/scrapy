6debad092698e422eef5cf7074215dc6288b6e80
Sean <yycyjqc@gmail.com>
2019-03-05 21:56:09 -0800

    typo

--- Python Script/example/example/spiders/book_spider.py
+++ Python Script/example/example/spiders/book_spider.py
@@ -11,20 +11,19 @@ class BooksSpider(scrapy.Spider):
         # retrieve data
         # the information of each book are under the <article class='product_pod]'
         # we are using css() to lookup all the article elements
-        for book in response.cass('article.product_pod'):
+        for book in response.css('article.product_pod'):
             # books' names are under 'article>h3>a>title'
-            name = book.xpath('.h3/a/@title').extract_first()
-
+            name = book.xpath('./h3/a/@title').extract_first()
             # the price is under <p class='price_color'>
             price = book.css('p.price_color::text').extract_first()
             yield{
                 'name': name,
                 'price': price,
             }
+
         # retrieve the url
         # the 'Next Page's url is under the ul.paper > li.next >a
         next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
-
         if next_url:
             # if find the next ulr, get the absolute path, establish the new request target
             next_url = response.urljoin(next_url)
