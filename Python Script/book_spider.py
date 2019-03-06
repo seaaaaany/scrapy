448c217463efab67d74a3afb51ce57090d3d0092
Sean <yycyjqc@gmail.com>
2019-03-05 20:36:58 -0800

    book spider

---
+++ Python Script/book_spider.py
@@ -0,0 +1,32 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    # identify each spider
+    name = 'books'
+
+    # set the url
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, respone):
+        # retrieve data
+        # the information of each book are under the <article class='product_pod]'
+        # we are using css() to lookup all the article elements
+        for book in response.cass('article.product_pod'):
+            # books' names are under 'article>h3>a>title'
+            name = book.xpath('.h3/a/@title').extract_first()
+
+            # the price is under <p class='price_color'>
+            price = book.css('p.price_color::text').extract_first()
+            yield{
+                'name': name,
+                'price': price,
+            }
+
+    # retrieve the url
+    # the 'Next Page's url is under the ul.paper > li.next >a
+    next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
+    if next_uil:
+        # if find the next ulr, get the absolute path, establish the new request target
+        next_url = response.urljoin(next_url)
+        yeild scrapy.Request(next_url, callback=self.parse)
